5eb95e5516ca84fcac0db8302c0492b214643ca3
Harshavardhan <gharsha086@gmail.com>
2017-03-08 11:33:58 -0800

    show user report full implimentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,11 +35,13 @@ end
 weeks_with_no_status = TimeEntry.where(status_id: nil).select(:week_id).collect { |w| w.week_id }.uniq
 
 weeks_with_no_status.each do |w_id|
-  TimeEntry.where(week_id: w_id).each do |te|
-    te.status_id = Week.find(w_id).status_id
-    te.approved_by = Week.find(w_id).approved_by
-    te.approved_by = Week.find(w_id).approved_date
-    te.save!
+  if !w_id.nil?
+    TimeEntry.where(week_id: w_id).each do |te|
+      te.status_id = Week.find(w_id).status_id
+      te.approved_by = Week.find(w_id).approved_by
+      te.approved_by = Week.find(w_id).approved_date
+      te.save!
+    end
   end
 end
 
