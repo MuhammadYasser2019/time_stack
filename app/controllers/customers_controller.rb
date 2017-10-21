2b1a7cefe2c696c674f0fb91c5b74b6880bbdc21
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2017-10-21 23:13:32 +0530

    bug-158: user invite issue

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -135,15 +135,17 @@ class CustomersController < ApplicationController
   
   def invite_to_project
     logger.debug "INVITED BY #{params[:invited_by_id]}"
+    project = Project.find(params[:project_id])
+
     @user = User.invite!(email: params[:email], :invitation_start_date => params[:invite_start_date],:employment_type => params[:employment_type], invited_by_id: params[:invited_by_id].to_i, pm: params[:project_manager])
-    @user.update(invited_by_id: params[:invited_by_id], customer_id: current_user.id)
+    @user.update(invited_by_id: params[:invited_by_id], customer_id: project.customer_id)
     pu = ProjectsUser.new
     # @users_on_project = @project.users
     # @users_on_project = params[:user_id]
     # @project = Project.find(1)
 
     user = User.find(@user.id)
-    project = Project.find(params[:project_id])
+    
     if project.users.include?(user)
       
     else
