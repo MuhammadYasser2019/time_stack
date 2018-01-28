f5736f8b8d0fd75142dc891d4379f02fc09b4181
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2018-01-28 21:39:43 +0530

    Migration issue

--- db/migrate/20180125220920_add_vacation_type_in_time_entries.rb
+++ db/migrate/20180125220920_add_vacation_type_in_time_entries.rb
@@ -4,17 +4,19 @@ class AddVacationTypeInTimeEntries < ActiveRecord::Migration[5.0]
 
     TimeEntry.reset_column_information
   	TimeEntry.all.each do |t|
-	  u = User.find t.user_id
-	  if u.customer_id.present?
-	  	c = Customer.find u.customer_id
-	  	if t.sick?
-	  	  v = c.vacation_types.where("vacation_title =?", "Sick").first
-	  	  t.vacation_type_id = v.id
-	  	elsif t.personal_day?
-	  	  v = c.vacation_types.where("vacation_title =?", "Personal").first
-	  	  t.vacation_type_id = v.id
-	  	end
-	  	t.save!
+  	  if t.user_id.present?	
+		u = User.find t.user_id
+		if u.customer_id.present?
+		  c = Customer.find u.customer_id
+		  if t.sick?
+		  	v = c.vacation_types.where("vacation_title =?", "Sick").first
+		  	t.vacation_type_id = v.id
+		  elsif t.personal_day?
+		  	v = c.vacation_types.where("vacation_title =?", "Personal").first
+		  	t.vacation_type_id = v.id
+		  end
+		  t.save!
+		end
 	  end
   	end
   end
