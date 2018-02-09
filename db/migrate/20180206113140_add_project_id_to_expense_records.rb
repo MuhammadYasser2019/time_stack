add5f7208b6d77c69f71fcb21fc72bf8e55728fc
susmitha9542 <susmitha.tinku@gmail.com>
2018-02-09 15:53:59 -0500

    Deleted migration files

--- db/migrate/20180206113140_add_project_id_to_expense_records.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIdToExpenseRecords < ActiveRecord::Migration
-  def change
-    add_column :expense_records, :project_id, :integer
-  end
-end
--- db/migrate/20180208184611_remove_attachment_from_expense_records.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAttachmentFromExpenseRecords < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :expense_records, :attachment, :string
-  end
-end
