ee27425edeee19b593f2f7b588f55ae124b04790
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2018-03-21 22:29:21 +0530

    bug-188

--- config/routes.rb
+++ config/routes.rb
@@ -97,7 +97,7 @@ Rails.application.routes.draw do
   
   get "/users/:id/proxies/:proxy_id" => "users#proxy_users"
   get "/users/:id/proxies/:proxy_id/enter_timesheets" => "users#enter_timesheets"
-  get "/users/:id/proxies/:proxy_id/show_timesheet_dates" => "users#show_timesheet_dates"
+  match "/users/:id/proxies/:proxy_id/show_timesheet_dates" => "users#show_timesheet_dates", via: [:get, :post]
   get "/users/:id/proxies/:proxy_id/fill_timesheet" => "users#fill_timesheet"
   get "/users/:id/proxies/:proxy_id/add_proxy_row" => "users#add_proxy_row", as: :add_proxy_row
   
