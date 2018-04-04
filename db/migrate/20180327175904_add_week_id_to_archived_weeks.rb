8fc74b25a7f7df96fbbb7f4eebf2df7fdf2ef13c
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2018-04-04 09:59:57 +0530

    fixing migration issue

--- db/migrate/20180327175904_add_week_id_to_archived_weeks.rb
+++
@@ -1,5 +0,0 @@
-class AddWeekIdToArchivedWeeks < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :archived_weeks, :week_id, :integer
-  end
-end
--- db/migrate/20180329184854_changed_reset_information.rb
+++
@@ -1,6 +0,0 @@
-class ChangedResetInformation < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :archived_weeks,:reset_date
-  	add_column :archived_weeks, :reset_date, :datetime
-  end
-end
