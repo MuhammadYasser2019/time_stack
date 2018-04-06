aa4f977365638d347ed0c588f508ce33218cecb8
Mbartlett413 <Mbartlett413@me.com>
2018-04-06 13:56:29 -0400

    adding other files

---
+++ app/mailers/feedback_mailer.rb
@@ -0,0 +1,14 @@
+class FeedbackMailer < ActionMailer::Base
+	default to: "Mbartlett413@me.com"
+	default from: "noreply@gmail.com"
+
+	def question_email(email, type, notes)
+		logger.debug("LOOKING FOR THE EMAIL #{email}")
+		@email = email
+		@type = type
+		@notes = notes
+
+		mail(subject:"You have Feedback")
+
+	end 
+end
