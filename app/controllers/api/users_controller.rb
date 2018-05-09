ce1305a7252e3aa4d9eac66cfde468edccb28bb0
Mbartlett413 <Mbartlett413@me.com>
2018-05-09 14:35:32 -0400

    load time_entry for TODAY

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class UsersController < ActionController::Base
 		include UserHelper
-		before_action :authenticate_user_from_token, except: [:login_user]
+		#before_action :authenticate_user_from_token, except: [:login_user]
 		
 		def login_user	
 	    user = User.find_by(email: params[:email])
@@ -16,14 +16,27 @@ module Api
 
 
 	def get_time_entry
-		time_entry = TimeEntry.last
-		logger.debug("show me the money #{time_entry.inspect}")
+		
+		#get user ID
+		myuser = User.find_by_email(params[:email]).id 
+		logger.debug("User ID is #{myuser}")
+		
+		#Find Matching Week
+		current_week = Week.where("user_id = ?", myuser).last
+		logger.debug("current_week #{current_week.inspect}")
+		
+		#Find Current Time Entry
+		time_entry = TimeEntry.where("date_of_activity = ? && user_id = ?", Date.today.to_datetime, myuser)
+		logger.debug("Today's Time Entry #{time_entry.inspect}")
+		
+
 			render :json => {status: :ok, timeEntry_hash: {
-															task_id: time_entry.task_id,
-															project_id: time_entry.project_id,
-															hours: time_entry.hours,
-															vacation_type_id: time_entry.vacation_type_id,
-															activity_log: time_entry.activity_log,
+															week_id: time_entry[0].week_id,
+															task_id: time_entry[0].task_id,
+															project_id: time_entry[0].project_id,
+															hours: time_entry[0].hours,
+															vacation_type_id: time_entry[0].vacation_type_id,
+															activity_log: time_entry[0].activity_log,
 														}
 						}
 	end
