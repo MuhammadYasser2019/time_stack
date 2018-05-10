135241329952cf41e78d3dde76852ce6b3bbfef9
Mbartlett413 <Mbartlett413@me.com>
2018-05-10 18:04:47 -0400

    added query to pass the 7time_enteries so the user can see 7dates avaliable to edit. Submit feature not done yet

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,14 +3,14 @@ module Api
 		include UserHelper
 		#before_action :authenticate_user_from_token, except: [:login_user]
 		
-		def login_user	
+	def login_user	
 	    user = User.find_by(email: params[:email])
 	    logger.debug("the user email you sent is : #{params[:email]}")
 	
-			if user&.valid_password?(params[:password])
-      	render json: user.as_json(only: [:email, :authentication_token, message: "success"]),status: :created
-    	else
-      	render :json => {status: :unauthorized ,message: "The email or password was incorrect. Please try again"}
+		if user&.valid_password?(params[:password])
+	      	render json: user.as_json(only: [:email, :authentication_token, message: "success"]),status: :created
+	    else
+	      	render :json => {status: :unauthorized ,message: "The email or password was incorrect. Please try again"}
     	end
     end 
 
@@ -20,17 +20,18 @@ module Api
 		#get user ID
 		myuser = User.find_by_email(params[:email]).id 
 		logger.debug("User ID is #{myuser}")
-		
-		#Find Matching Week
-		current_week = Week.where("user_id = ?", myuser).last
-		logger.debug("current_week #{current_week.inspect}")
+	
 		
 		#Find Current Time Entry
 		time_entry = TimeEntry.where("date_of_activity = ? && user_id = ?", Date.today.to_datetime, myuser)
 		logger.debug("Today's Time Entry #{time_entry.inspect}")
 		
 
-		render :json => {status: :ok, timeEntry_hash: {
+		#TimeEntries To Load Into DropDown
+		avaliable_entries = TimeEntry.where("week_id = ?", time_entry[0].week_id).collect{|w| w.date_of_activity.strftime("%Y/%m/%d")}
+		logger.debug("These are entries Avaliable #{avaliable_entries.inspect}")
+
+				render :json => {status: :ok, timeEntry_hash: {
 														id: time_entry[0].id,
 														week_id: time_entry[0].week_id,
 														task_id: time_entry[0].task_id,
@@ -38,12 +39,14 @@ module Api
 														hours: time_entry[0].hours,
 														vacation_type_id: time_entry[0].vacation_type_id,
 														activity_log: time_entry[0].activity_log,
-													}
-					}
+													},
+													date_of_activity: avaliable_entries
+
+				}
+
 	end
 
 	def post_data
-
 		te = TimeEntry.find_by_id(params[:id])
 		logger.debug("Are you finding me #{te}")
 		te.project_id = params[:project_id]
@@ -54,8 +57,5 @@ module Api
 		te.save
 	end 
 
-
-		
-
-	end
+end 
 end
