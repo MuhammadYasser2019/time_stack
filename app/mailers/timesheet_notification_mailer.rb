39b275490f7d251d347afdce8c068b3cb94f8d3d
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2018-09-01 23:10:52 +0530

    mail changes for notification

--- app/mailers/timesheet_notification_mailer.rb
+++ app/mailers/timesheet_notification_mailer.rb
@@ -1,6 +1,6 @@
 class TimesheetNotificationMailer < ActionMailer::Base
 
-	default from: 'timestackrsi@gmail.com'
+	default from: 'technicalsupport@resourcestack.com'
 
 
 	def mail_to_user(week, user)
