9b820640a2c95bb4825f536f744aa551103ac74a
Sparsha Bhattarai <mail.sparsha@gmail.com>
2020-04-08 10:36:43 -0400

    Fix issues with master merge.

--- db/migrate/20190208112020_create_external_configurations.rb
+++ db/migrate/20190208112020_create_external_configurations.rb
@@ -1,14 +1,16 @@
 class CreateExternalConfigurations < ActiveRecord::Migration[5.0]
   def change
-    create_table :external_configurations do |t|
-      t.integer :customer_id
-      t.string :system_type
-      t.string :url
-      t.string :jira_email
-      t.string :password
-      t.string :confirm_password
-      t.integer :created_by
-      t.timestamps
-    end
+    if !table_exists?(:external_configurations)
+      create_table :external_configurations do |t|
+        t.integer :customer_id
+        t.string :system_type
+        t.string :url
+        t.string :jira_email
+        t.string :password
+        t.string :confirm_password
+        t.integer :created_by
+        t.timestamps
+      end
+  end
   end
 end
