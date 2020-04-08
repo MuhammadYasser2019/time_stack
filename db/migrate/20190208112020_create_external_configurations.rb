bd9ecb404db54917de907c6baed9d620f07248bb
Sparsha Bhattarai <mail.sparsha@gmail.com>
2020-04-08 12:38:03 -0400

    Revert changes

--- db/migrate/20190208112020_create_external_configurations.rb
+++ db/migrate/20190208112020_create_external_configurations.rb
@@ -1,16 +1,14 @@
 class CreateExternalConfigurations < ActiveRecord::Migration[5.0]
   def change
-    if !table_exists?(:external_configurations)
-      create_table :external_configurations do |t|
-        t.integer :customer_id
-        t.string :system_type
-        t.string :url
-        t.string :jira_email
-        t.string :password
-        t.string :confirm_password
-        t.integer :created_by
-        t.timestamps
-      end
-  end
+    create_table :external_configurations do |t|
+      t.integer :customer_id
+      t.string :system_type
+      t.string :url
+      t.string :jira_email
+      t.string :password
+      t.string :confirm_password
+      t.integer :created_by
+      t.timestamps
+    end
   end
 end
