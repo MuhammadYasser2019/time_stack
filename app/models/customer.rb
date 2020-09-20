40e0970234e993698ec692ca0c79176250f5bef4
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2020-09-20 12:23:05 +0530

    fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -149,15 +149,14 @@ class Customer < ApplicationRecord
         shifts.each do |s|
           submitted_time = 0.0
           approved_time = 0.0
-          s.project_shifts.each do |ps|
-            project_hash[p.id][t.id][s.id] ||= []
-            submitted_time += TimeEntry.where(project_shift_id: ps.id, task_id: t.id, project_id: p.id, date_of_activity: start_day..end_date, status_id: 2).order(:date_of_activity).sum(:hours)
-            approved_time += TimeEntry.where(project_shift_id: ps.id, task_id: t.id, project_id: p.id, date_of_activity: start_day..end_date, status_id: 3).order(:date_of_activity).sum(:hours)
+          s.project_shifts.where(project_id: p.id).each do |ps|
+            project_hash[p.id][t.id][ps.id] ||= []
+            submitted_time += TimeEntry.where(project_shift_id: ps.id, task_id: t.id, project_id: p.id, status_id: 2).order(:date_of_activity).sum(:hours)
+            approved_time += TimeEntry.where(project_shift_id: ps.id, task_id: t.id, project_id: p.id, status_id: 3).order(:date_of_activity).sum(:hours)
+            project_hash[p.id][t.id][ps.id] << approved_time
+            project_hash[p.id][t.id][ps.id] << submitted_time
           end
-          if project_hash[p.id][t.id][s.id].present?
-            project_hash[p.id][t.id][s.id] << approved_time
-            project_hash[p.id][t.id][s.id] << submitted_time
-          end
+         
         end
       end
     end
