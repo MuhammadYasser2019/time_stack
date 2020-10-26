9c453dc5dde1a198d046e2147b9e9416ded55700
nitinchauhan1912 <engr.chauhannitin@gmail.com>
2020-10-27 01:36:00 +0530

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
   def find_faq
     if current_user.present? && current_user.pm?
       @faq = Feature.where(feature_type: "FAQ for Project Manager").last
-    elsif current_user.present? &&  current_user.pm?
+    elsif current_user.present? &&  current_user.cm?
       @faq = Feature.where(feature_type: "FAQ for Customer Manager").last
     elsif current_user.present?
       @faq = Feature.where(feature_type: "FAQ for User").last
